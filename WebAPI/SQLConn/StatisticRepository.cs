ef7c91fea696b02be612ef74f041c3929c3aa058
ZealousCynic <zealouscynic@gmail.com>
2019-12-20 15:07:17 +0100

    GetByID statistic implemented

---
+++ WebAPI/SQLConn/StatisticRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class StatisticRepository : SQLRepository<Statistic>
+    {
+        public override void Create(Statistic entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(Statistic entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Statistic[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Statistic GetById(int id)
+        {
+            CmdText = "GetLatestReadings";
+
+            Cmd.Connection = Connection;
+            Cmd.CommandText = CmdText;
+
+            Cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlParameter[] sqlParams = new[]
+            {
+            new SqlParameter("@VortexId", id),
+            new SqlParameter("@Temperature", System.Data.SqlDbType.Int),
+            new SqlParameter("@Compas", System.Data.SqlDbType.Int),
+            new SqlParameter("@WaterLevel", System.Data.SqlDbType.Int),
+            new SqlParameter("@Powerstate", System.Data.SqlDbType.Bit),
+            new SqlParameter("@Drivestate", System.Data.SqlDbType.Bit),
+            };
+
+            //Skip param 1 in loop - not output.
+            for (int i = 1; i < sqlParams.Length; i++)
+                sqlParams[i].Direction = System.Data.ParameterDirection.Output;
+
+            Cmd.Parameters.AddRange(sqlParams);
+
+            Cmd.Connection.Open();
+            Cmd.ExecuteNonQuery();
+
+            return new Statistic
+            {
+                Temperature = (int)sqlParams[1].Value,
+                Compas = (int)sqlParams[2].Value,
+                WaterLevel = (int)sqlParams[3].Value,
+                Powerstate = (bool)sqlParams[4].Value,
+                Drivestate = (bool)sqlParams[5].Value
+            };
+        }
+
+        public override void Update(Statistic entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WebAPI/WebAPI/StatisticConverter.cs
+++ WebAPI/WebAPI/StatisticConverter.cs
@@ -19,6 +19,23 @@ namespace WebAPI
             };
         }
 
+        public static Models.Statistic[] ConvertTo(DAL.Statistic[] s)
+        {
+            Models.Statistic[] toReturn = new Models.Statistic[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+                toReturn[i] = new Models.Statistic
+                {
+                    Temperature = s[i].Temperature,
+                    Compas = s[i].Compas,
+                    WaterLevel = s[i].WaterLevel,
+                    Powerstate = s[i].Powerstate,
+                    Drivestate = s[i].Drivestate
+                };
+
+            return toReturn;
+        }
+
         public static Models.Statistic ConvertTo(DAL.Statistic s)
         {
             return new Models.Statistic
